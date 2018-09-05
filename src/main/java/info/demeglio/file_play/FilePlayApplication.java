2ce3847191cc5f3f45a8edc906dc4fa61e083953
Anthony J. DeMeglio <22527727+ademeglio@users.noreply.github.com>
2018-09-04 21:23:18 -0400

    add template directory to work with

---
+++ src/main/java/info/demeglio/file_play/FilePlayApplication.java
@@ -0,0 +1,10 @@
+package info.demeglio.file_play;
+
+public class FilePlayApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/info/demeglio/file_play/FilePlayTest.java
@@ -0,0 +1,12 @@
+package info.demeglio.file_play;
+
+import org.junit.Test;
+
+public class FilePlayTest {
+
+	@Test
+	public void shouldCreateADirectory() {
+		
+	}
+	
+} // End FilePlayTest()
